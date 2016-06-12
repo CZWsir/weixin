f400468689a08a12753fd0995bba08774a194704
chenzewei <chenzewei@chenzeweidemacbook-pro.local>
2016-06-11 18:46:23 -0700

    初始化项目结构

---
+++ momo/momo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  momo
+//
+//  Created by chenzewei on 16/6/11.
+//  Copyright © 2016年 chenzewei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ momo/momo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  momo
+//
+//  Created by chenzewei on 16/6/11.
+//  Copyright © 2016年 chenzewei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
